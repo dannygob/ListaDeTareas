c78613075a251c7b9859b7f2e4a3633b680f8c53
tardes <dannygomezb@gmail.com>
2025-04-30 19:41:20 +0200

    5th comit

--- app/src/main/java/com/example/listadetareas/CategoryDAO.kt
+++ app/src/main/java/com/example/listadetareas/CategoryDAO.kt
@@ -2,12 +2,14 @@ package com.example.listadetareas
 
 import android.content.ContentValues
 import android.content.Context
+import android.util.Log
+import kotlin.math.log
 
 class CategoryDAO (private val context: Context) {
 
-   private lateinit var  db: SQLiteDatabase
+    private lateinit var db: SQLiteDatabase
 
-    private fun open () {
+    private fun open() {
         db = DatabaseManager(context).writableDatabase
     }
 
@@ -16,21 +18,58 @@ class CategoryDAO (private val context: Context) {
     }
 
     //insertar
-    fun insert(category: Category){
-    // Gets the data repository in write mode
-    open()
+    fun insert(category: Category) {
+        // Gets the data repository in write mode
+        open()
+
+        try{
 
         // Create a new map of values, where column names are the keys
 
         val values = ContentValues()
-            values.put(Category.COLUNM_NAME_TITLE, category.title)
+        values.put(Category.COLUMN_NAME_TITLE, category.title)
 
         // Insert the new row, returning the primary key value of the new row
-        val newRowId= db.insert (Category.TABLE_NAME, null, values)
+        val newRowId = db.insert(Category.TABLE_NAME, null, values)
+        Log.i("DATABASE", "Insert a category with id: $newRowId")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    } finally {
+        close()
     }
-
+}
 
     //Actualizar
+
+
+fun udate (category: Category){
+    open()
+
+    try{
+        // Create a new map of values, where column names are the keys
+        val values = ContentValues()
+        values.put (Category.COLUMN_NAME_TITLE,category.title)
+
+
+        //which row to update, base on titleç
+
+        val selection = "${Category.COLUMN_NAME_ID}= ${category.id}"
+
+        val selectionArgs= arrayOf("MyOldTitle")
+
+
+        val count = db.update (
+            Category.TABLE_NAME,
+            values,
+            selection,
+            selectionArgs)
+
+    Log.i("DATABASE", "Udate category with id: $newRowId")
+} catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            close()
+        }
 
     //Borrar
 
