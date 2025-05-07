97c956bafb01832549705be3cd3056533e2a0c8e
tardes <dannygomezb@gmail.com>
2025-05-07 17:52:49 +0200

    8th commit

--- app/src/main/java/com/example/listadetareas/data/CategoryDAO.kt
+++ app/src/main/java/com/example/listadetareas/data/CategoryDAO.kt
@@ -4,7 +4,7 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.util.Log
-import com.example.listadetareas.manager.DatabaseManager
+import com.example.listadetareas.utils.DatabaseManager
 //import com.example.listadetareas.utils.DatabaseManager
 
 class CategoryDAO(private val context: Context) {
