13aa1775559d4fbb669bd3e929ee4db4e0dabdb0
tardes <dannygomezb@gmail.com>
2025-05-07 19:32:05 +0200

    10th commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.listadetareas"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.listadetareas"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
