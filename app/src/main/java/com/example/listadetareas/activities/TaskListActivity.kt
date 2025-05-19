70a9d14d807c6e257e78acde84f8365ebdbbac17
tardes <dannygomezb@gmail.com>
2025-05-19 20:03:44 +0200

    4to comit

--- app/src/main/java/com/example/listadetareas/activities/TaskListActivity.kt
+++ app/src/main/java/com/example/listadetareas/activities/TaskListActivity.kt
@@ -203,8 +203,10 @@ fun showTaskMenu(position: Int, v: View) {
 }
 
 fun configureGesture() {
-    val gesture = ItemTouchHelper(object :
-        ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN) {)
+    val gestures = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+        ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+        ItemTouchHelper.LEFT or ItemTouchHelper.DOWN
+    ) {)
         override fun onMove(
             recyclerView: RecyclerView,
             viewHolder: RecyclerView.ViewHolder,
